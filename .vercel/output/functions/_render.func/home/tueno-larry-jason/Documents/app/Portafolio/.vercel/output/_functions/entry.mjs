0a7e24555ed89b436109680ae8ea0df0f1b66a3e
tueno-larry-jason <tuenojason@proton.me>
2025-03-22 17:06:20 +0100

    change node version

---
+++ .vercel/output/functions/_render.func/home/tueno-larry-jason/Documents/app/Portafolio/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,29 @@
+import { renderers } from './renderers.mjs';
+import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
+import { c as createExports } from './chunks/entrypoint_DRsWSQM2.mjs';
+import { manifest } from './manifest_tzT_Q3WS.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["../../../.nvm/versions/node/v23.10.0/lib/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "c94286ac-b957-46ce-bfcc-5eae71c613af",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
